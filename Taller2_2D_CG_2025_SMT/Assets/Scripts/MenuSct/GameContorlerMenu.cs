a581322487125694b5bb82744b2dc763234a413b
Mateolpz <165231201+Mateolpz@users.noreply.github.com>
2025-09-30 17:27:06 -0500

    .
    Mejore la decoracion y la forma del nivel de bosque

--- Taller2_2D_CG_2025_SMT/Assets/Scripts/MenuSct/GameContorlerMenu.cs
+++ Taller2_2D_CG_2025_SMT/Assets/Scripts/MenuSct/GameContorlerMenu.cs
@@ -6,27 +6,27 @@ using TMPro;
 public class GameContorlerMenu : MonoBehaviour
 {
     [Header("UI Panels")]
-    public GameObject instruccionesPanel; // arrastra tu Panel en el inspector
+    public GameObject instruccionesPanel; //arrastra tu Panel en el inspector
 
     private void Update()
     {
         
     }
 
-    // 🔹 Cambiar de escena
+    //Cambiar de escena
     public void LoaderSceneM(string NameScene)
     {
         SceneManager.LoadScene(NameScene);
     }
 
-    // 🔹 Mostrar panel de instrucciones
+    //Mostrar panel de instrucciones
     public void ShowInstrucciones()
     {
         if (instruccionesPanel != null)
             instruccionesPanel.SetActive(true);
     }
 
-    // 🔹 Ocultar panel de instrucciones
+    //Ocultar panel de instrucciones
     public void HideInstrucciones()
     {
         if (instruccionesPanel != null)
